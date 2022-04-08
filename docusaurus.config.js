2982efefb9ab48f697b231fe73938506eb0a5cf8
hestudy <1467602180@qq.com>
2022-04-09 03:18:49 +0800

    部署

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "D3js学习记录",
   tagline: "全中文记录我的D3学习过程和实例",
-  url: "https://1467602180.github.io/d3-learning-record",
+  url: "https://1467602180.github.io",
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
